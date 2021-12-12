c0e888029b7361885d824663ac96ee6773b874b4
Clint Gunter <clint@clint.io>
2021-12-12 16:18:54 -0600

    Added authetication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentications#login'
+  get '/auth/verify', to: 'authentications#verify'
   resources :cpi_data
   resources :entries
   resources :users
