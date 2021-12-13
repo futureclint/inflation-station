dc28a98e14a6d46c75a3f4992902e059725f0167
Clint Gunter <clint@clint.io>
2021-12-12 20:31:14 -0600

    Updated routes for CPI data and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
-  resources :cpi_data
+  resources :cpi_data, only: :index
   resources :entries
-  resources :users
+  resources :users, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
