d0a5c8afcecf89e43745c1a5a7ad68f3cbffd0db
Clint Gunter <clint@clint.io>
2021-12-14 15:08:14 -0600

    Add min-height to main page element, and add basic form to calculate page

--- client/src/screens/Calculate.jsx
+++ client/src/screens/Calculate.jsx
@@ -2,6 +2,15 @@ export default function Calculate() {
   return (
     <>
       <h2>Calculate</h2>
+      <form>
+        <label>Starting Year</label>
+        <input />
+        <label>Starting Amount $</label>
+        <input />
+        <label>Ending Year</label>
+        <input />
+        <button>Calculate Ending Amount</button>
+      </form>
     </>
   )
 }
