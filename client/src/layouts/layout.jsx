f6906c7c953ff31a3cf5800ae55e2403325194a8
Clint Gunter <clint@clint.io>
2021-12-14 05:54:52 -0600

    Added basic layout that renders

--- client/src/layouts/layout.jsx
+++ client/src/layouts/layout.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Layout() {
+export default function Layout(props) {
   return (
     <div>
-
+      <header>
+        <h1>Inflation Station</h1>
+        <nav>
+          <Link>Login/Register</Link>
+        </nav>
+      </header>
+      {props.children}
     </div>
   )
 }
